183e8d857c065c192151e0ddb4c7af150cd0af90
HWYS <htet.waiyan.swe@myancare.org>
2022-01-07 15:08:19 +0630

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Htet Wai Yan Swe on 1/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
